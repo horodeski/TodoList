46492e1cb59013ce6a2c621ad0871fd3d7a81a00
horodeski <geovana.horodeski06@gmail.com>
2023-10-14 13:30:00 -0300

    Estrutura inicial

---
+++ src/test/java/br/com/horodeski/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.horodeski.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
